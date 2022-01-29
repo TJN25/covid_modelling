4333935c823a8746c953b233d546da4b7e06f43d
TJN25 <nicholson.tom.n@gmail.com>
2022-01-29 16:31:19 +1300

    changed the defaults to my current testing values

--- covid_modelling/server.R
+++ covid_modelling/server.R
@@ -174,7 +174,7 @@ server <- function(input, output, session) {
   # })
   output$datatab <- renderDataTable({
     
-    DT::datatable( vaccine_data())
+    DT::datatable( vaccine_data_2())
     
   })   
   
